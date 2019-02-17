eb17380841c6e088ae37b3d817d92e22daa3ee2f
noel <noelperal@yahoo.es>
2019-02-17 15:55:59 +0100

    .

--- trabajo/netsg.bat
+++ trabajo/netsg.bat
@@ -5,7 +5,7 @@ REM lista tanto ficheros abiertos como conexiones cada 5 segundos (con filtros p
 REM REQUIERE: psfile.exe y grep.exe
 
 :D
-		rem :5357 -> host discovery
+		rem :5357 -- host discovery
 	psfile.exe | grep -v Locks | grep -v Access | grep -v "Files opened"
 	netstat -b -o -p TCP | grep -v http | grep -v :5357 | grep -v Proto | grep -v hercules | grep -v proxy | grep -v propiedad | grep -v activas | grep -v PAT_NET | grep -v OUTLOOK | grep -v spoolsv | grep -v IEXPLORE | grep -v iexplore | grep -v titan | grep -v ABSOLON | grep -v 7570-EADS | cut -d: -f2,3 | grep -v tcscd | grep -v corvus | grep -v tcsex | grep -v tcspriv | grep -v tcsfsv | grep -v tcspsw | grep -v mstsc. | grep -v tcsepov
 
