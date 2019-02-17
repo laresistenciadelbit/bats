963b3e54f8148ea8926fa21700b7b546fa01ad74
laresistenciadelbit <44118971+laresistenciadelbit@users.noreply.github.com>
2019-02-17 15:51:55 +0100

    Delete mac.bat

--- trabajo/mac.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM cambia tu mac a otra
-
-ping 127.0.0.1>nul
-
-set mac=002170db18ae
-
-reg add hklm\system\controlset001\control\class\{4D36E972-E325-11CE-BFC1-08002bE10318}\0001 /t reg_sz /v NetworkAddress /d %mac% /f
